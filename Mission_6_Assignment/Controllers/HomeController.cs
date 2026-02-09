c09c63cbb4a200f62fce9462e86142a1bad3cc61
psandstrom14 <psandstrom8@gmail.com>
2026-02-09 08:49:11 -0700

    Add project files.

---
+++ Mission_6_Assignment/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Mission_6_Assignment.Models;
+
+namespace Mission_6_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
